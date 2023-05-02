292e7950f98fa77b3a67cd441bd25b287a166e1a
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-05-02 11:24:33 -0400

    scripts update
    Deleted old lines in scripts.js
    added in a new array containing pokemon information

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-;alert('Hello world');
-let favoriteFood='Steak';
-document.write(favoriteFood);
+let pokemonList=[
+    {name: 'Turtwig', height: '0.4m', type:'Grass'},
+    {name: 'Umbreon', height: '1m', type: 'Dark'},
+    {name: 'Gengar', height: '1.5m', type: 'Ghost, Poison'}
+]
