bb2a72ff5a7695d41ebb5d580c4f0e2c157289c9
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-05-04 12:02:33 -0400

    forEach
    Updated for loop to change to forEach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,7 @@
 let pokemonList=[
     {name: "Turtwig", height: 0.4, type:["Grass"]},
     {name: "Umbreon", height: 1, type: ["Dark"]},
-    {name: "Gengar", height: 1.5, type: ["Ghost", "Poison"]}
-];
-for (let i=0; i <pokemonList.length; i++){
-    //using to assignn any Pokemon larger than 1 the Wow, that is big text.
-    if (pokemonList[i].height > 1){
-        document.write(pokemonList[i].name + " " + "(Height: " + pokemonList[i].height + ") - Wow, that is big! <br>")
-    }
-    else{
-        document.write(pokemonList[i].name + " " + "(Height: " + pokemonList[i].height + ") <br>");
-    }
-}
-console.log(pokemonList);
-
+    {name: "Gengar", height: 1.5, type: ["Ghost", "Poison"]}];
+pokemonList.forEach(function(pokemon){
+    console.log(pokemon);
+});
