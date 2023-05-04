4bfa7ce0b91ea89ea9c6ffffb7d7533f85e4756c
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-05-04 13:27:12 -0400

    IIFE
    Added IIFE and updated the foreach to return the function inside the iife

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
-let pokemonList=[
-    {name: "Turtwig", height: 0.4, type:["Grass"]},
-    {name: "Umbreon", height: 1, type: ["Dark"]},
-    {name: "Gengar", height: 1.5, type: ["Ghost", "Poison"]}];
-pokemonList.forEach(function(pokemon){
-    console.log(pokemon);
+let pokemonRepository = (function(){
+    let pokemonList=[
+        {name: "Turtwig", height: 0.4, type:["Grass"]},
+        {name: "Umbreon", height: 1, type: ["Dark"]},
+        {name: "Gengar", height: 1.5, type: ["Ghost", "Poison"]}
+    ];
+        function add(pokemon){
+            pokemonList.push(pokemon);
+        }
+        function getAll(){
+        return pokemonList;
+        }
+        return{
+            add: add,
+            getAll:  getAll
+    };
+})();
+pokemonRepository.add({ name: "Pikachu"});
+pokemonRepository.getAll().forEach(function (pokemon){
+    console.log(pokemon)
 });
+console.log(pokemonRepository.getAll());
