74225fcffb3f83632fdd887ded8fd9fe36fed646
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-05-05 19:49:59 -0400

    scripts.js
    Updated some typing to give a cleaner look overall.
    Added addListItem to display pokemon in a clickable list and the output being shown in the console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonRepository = (function(){
     let pokemonList=[
-        {name: "Turtwig", height: 0.4, type:["Grass"]},
-        {name: "Umbreon", height: 1, type: ["Dark"]},
-        {name: "Gengar", height: 1.5, type: ["Ghost", "Poison"]}
+        {name: "Turtwig", height: 0.4, type:"Grass"},
+        {name: "Umbreon", height: 1, type: "Dark"},
+        {name: "Gengar", height: 1.5, type: "Ghost, Poison"},
     ];
     function add(pokemon){
         pokemonList.push(pokemon);
@@ -10,12 +10,30 @@ let pokemonRepository = (function(){
     function getAll(){
         return pokemonList;
     }
+    function showDetails (pokemon){
+        console.log(pokemon);
+    }
+    //Following modifies pokemon repository and turns the array into interacable buttons. While also modifying how they look through the css file.
+    function addListItem(pokemon){
+        let pokemons = document.querySelector('.pokemon-list');
+        let listPokemon = document.createElement('ul');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name, pokemon;
+        button.classList.add('button-class');
+        button.value = pokemon.name;
+        listPokemon.appendChild(button);
+        pokemons.appendChild(listPokemon);
+        button.addEventListener('click', function(event){
+            showDetails(pokemon.name)
+        });
+    }
     return{
         add: add,
-        getAll:  getAll
-    };
+        getAll:  getAll,
+        addListItem: addListItem
+    }
 })();
-pokemonRepository.add({ name: "Pikachu"});
+pokemonRepository.add({ name: "Pikachu", height: 0.3, type: "Electric"});
 pokemonRepository.getAll().forEach(function (pokemon){
-    console.log(pokemon)
+    pokemonRepository.addListItem(pokemon)
 });
